6e88c531065e04d5d67ce2898ca4259680ae78b8
Michelle <michellexia@utexas.edu>
2020-12-06 22:40:31 -0600

    updates

--- MergeHospitals.py
+++ MergeHospitals.py
@@ -1,126 +1,115 @@
 import pandas as pd
 import numpy as np
+from difflib import SequenceMatcher
 
-with open('hosp_cont_better.txt') as f:
-    cms_names = [line.rstrip() for line in f]
-print("length", len(cms_names))
+# inputs:
+CMS_file_name = 'pos_clia_csv_sep20/pos_clia_sep20.csv'
+breach_file_name = 'breach_report.csv'
+matched_cms_txt = 'matching_cms_names.txt'
+updated_breach_file_name = 'updated_breach.csv'
+merged_file_name = 'MergedCMSWithBreaches.csv'
 
-#cms_names.sort()
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
+def find_matches():
+    """
+    This function finds the CMS hospital names which match the breached hospitals
+    input: breach and CMS csv files
+    output: text file with CMS hospital names which match breached hospitals
+    """
+    # read breach and CMS csv
+    breachdf = pd.read_csv(breach_file_name, encoding='latin1')
+    breachdf.rename(columns=lambda x: x.strip(), inplace=True)
+    print(breachdf.isnull().sum().sum(), "null columns")
 
-breachdf = pd.read_csv('breach_report.csv', encoding='latin1')
-breachdf.rename(columns=lambda x: x.strip(), inplace=True)
-breachdf = breachdf.drop(columns=['Web Description'])
-#breachdf = breachdf.applymap(lambda x: x.strip() if type(x)==str else x)
-#breachdf = breachdf.sort_values(by=['FAC_NAME'])
-print(breachdf.isnull().sum().sum(), "null columns")
+    df = pd.read_csv(CMS_file_name, encoding='latin1')
+    df.rename(columns=lambda x: x.strip(), inplace=True)
+        
+    breachdf['FAC_NAME'] = breachdf['FAC_NAME'].str.upper()      
 
-df = pd.read_csv('pos_clia_csv_sep20/pos_clia_sep20.csv', encoding='latin1', dtype=str)
-df.rename(columns=lambda x: x.strip(), inplace=True)
-#df = df.applymap(lambda x: x.strip() if type(x)==str else x)
-#df = df.sort_values(by=['FAC_NAME'])
+    combo = []
+    df['FAC_NAME'] = df['FAC_NAME'].str.upper()
+    for i in range(len(breachdf)):
+        original = breachdf['FAC_NAME'][i]
+        # ".startswith" can also be changed to ".contains" but will give less matched rows
+        tmp = df[df["FAC_NAME"].str.startswith(original.split()[0])]['FAC_NAME'].values.tolist()
+        # find the most similar hospital
+        if tmp:
+            sim = [similar(original, val) for val in tmp]
+            maxtmp = max(sim)
+            combo.append(tmp[sim.index(maxtmp)])
+            print(tmp[sim.index(maxtmp)])
+        
+    print("CMS name list created", combo[0])
 
-print('dataframes created')
-breachdf['Breach Occured'] = 'No'
-for i in range(len(cms_names)):
-    for breachrow in breachdf['FAC_NAME']:
-        #print(breachrow, cms_names[i])
-        if breachrow.upper().split()[0] in cms_names[i]:
-            updated_name = cms_names[i]
-            print("update", updated_name)
-            print("old", breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'FAC_NAME'])
-            breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'Breach Occured'] = 'Yes'
-            breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'FAC_NAME'] = updated_name
-            print("new", breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'FAC_NAME'])
-print("done", breachdf)
-breachdf.to_csv('updated_breachdf2.csv', index=False)
+    # flatten list of CMS names
+    arr = np.array(combo)
+    combo = np.hstack(arr)
+    print("combo", combo[0])
 
+    # write CMS names to text file
+    with open(matched_cms_txt, 'a') as writer:
+        for hosp in combo:
+            writer.write(hosp + "\n")
+    print("written to " + matched_cms_txt)
 
+def update_breaches():
+    """
+    This function updatesthe  breach file with matched CMS hospital names
+    input: breach csv file name, CMS data file name
+    output: updated_breachdf2.csv
+    """
+    # load matched CMS names
+    with open(matched_cms_txt) as f:
+        cms_names = [line.rstrip() for line in f]
 
-"""df['Breach'] = "No"
-df['Covered Entity Type'] = None
-df['Individuals Affected'] = None
-df['Breach Submission Date'] = None
-df['Type of Breach'] = None
-df['Location of Breached Information'] = None
-df['Business Associate Present'] = None
+    # load CMS and Breach CSV files
+    breachdf = pd.read_csv(breach_file_name, encoding='latin1')
+    breachdf.rename(columns=lambda x: x.strip(), inplace=True)
+    breachdf = breachdf.drop(columns=['Web Description'])
+    print(breachdf.isnull().sum().sum(), "null columns")
+    df = pd.read_csv(CMS_file_name, encoding='latin1', dtype=str)
+    df.rename(columns=lambda x: x.strip(), inplace=True)
 
-print("breachdf")
-print(breachdf)
-print()
-searchstr = cms_names[1].split()[0]
-print("cmsnames", cms_names[0])
-print("breachdf", breachdf.loc[[0]])
+    # create new column for whether breach occured
+    breachdf['Breach Occured'] = 'No'
 
-for i in range(len(cms_names)):
-    for breachrow in breachdf['FAC_NAME']:
-        #print(breachrow, cms_names[i])
-        if breachrow.upper() in cms_names[i]:
-            print("cmsnames", cms_names[i])
-            print("index", i)
-            print("breachrow", breachrow)
-            print("appending", breachdf.loc[breachdf['FAC_NAME'] == breachrow])
-            tmp = breachdf.loc[breachdf['FAC_NAME'] == breachrow]
-            tmp = tmp.copy()
-            tmp = tmp.astype('string')
-            print("TMP", tmp['Covered Entity Type'])
+    # iterate through breaches and change hospital names to CMS names
+    for i in range(len(cms_names)):
+        for breachrow in breachdf['FAC_NAME']:
+            if breachrow.upper().split()[0] in cms_names[i]:
+                updated_name = cms_names[i]               
+                breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'Breach Occured'] = 'Yes'
+                breachdf.loc[breachdf['FAC_NAME'] == breachrow, 'FAC_NAME'] = updated_name
+    breachdf.to_csv(updated_breach_file_name, index=False)
 
-            ind = df.index[df['FAC_NAME'] == cms_names[i]]
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Breach'] = 'Yes'
-            a = tmp['Covered Entity Type'].astype('string')
-            #df['Breach'][ind] = "Yes"
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Covered Entity Type'] = a #tmp['Covered Entity Type'].astype('string')
-            print("DF", df['Covered Entity Type'][df['FAC_NAME'] == cms_names[i]])
-            # df['Covered Entity Type'][ind] = tmp['Covered Entity Type']
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Individuals Affected']= tmp['Individuals Affected'].astype('|S')
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Breach Submission Date'] = tmp['Breach Submission Date'].astype('|S')
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Type of Breach'] = tmp['Type of Breach'].astype('|S')
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Location of Breached Information'] = tmp['Location of Breached Information'].astype('|S')
-            df.loc[df['FAC_NAME'] == cms_names[i], 'Business Associate Present'] = tmp['Business Associate Present'].astype('|S')
-            print("final", df['FAC_NAME'][ind])
-            
+    print("Written to " + updated_breach_file_name)
 
-            #breachdf = breachdf.drop(breachdf.loc[breachdf['FAC_NAME'] == breachrow])
+def merge_breached():
+    """
+    This function merges the CMS data with the breached data
+    input: updated breach csv, CMS file
+    output: merged CSV file
+    """
+    # read breach and CMS data
+    breachdf = pd.read_csv(updated_breach_file_name, encoding='latin1')
+    breachdf.rename(columns=lambda x: x.strip(), inplace=True)
+    print(breachdf.isnull().sum().sum(), "null columns")
 
-#selected = breachdf.loc[breachdf['FAC_NAME'].values.str in cms_names[1]]
-print("matched", df)
-df.to_csv("CMSNameMatch.csv", index=False)
+    df = pd.read_csv(CMS_file_name, encoding='latin1')
+    df.rename(columns=lambda x: x.strip(), inplace=True)
 
-#mask2 = pd.Series(cms_names[1] in breachdf.FAC_NAME.values)
-#mask = pd.Series(breachdf.FAC_NAME.values).str.contains(cms_names[1], case=False).values
+    print("dataframes read")
 
-#print('mask', mask)
+    # merge data
+    new_df = df.merge(breachdf, left_on='FAC_NAME', right_on='FAC_NAME', how='outer')
+    print("merged", new_df)
 
-#a = breachdf.loc[:,mask]
+    new_df.to_csv(merged_file_name, index=False)
+    print("Written to", merged_file_name)
 
-#a = breachdf.loc[breachdf['FAC_NAME'].str.contains(cms_names[0])]
-#a = breachdf[breachdf['FAC_NAME'].str.startswith(cms_names[0])]"""
-"""print("a", a)
-
-
-df['Breach'] = "No"
-df['Covered Entity Type'] = None
-df['Individuals Affected'] = None
-df['Breach Submission Date'] = None
-df['Type of Breach'] = None
-df['Location of Breached Information'] = None
-df['Business Associate Present'] = None
-for i in range(len(df['FAC_NAME'])):
-    curr =  df['FAC_NAME'][i].upper()
-    if curr in cms_names:
-        ind = cms_names.index(curr)
-        print("name", cms_names[i])
-        mask = df[breachdf['FAC_NAME'].str.contains(cms_names[ind])]
-        print(mask)
-        print()
-        print(df[breachdf['FAC_NAME'].str.contains(cms_names[ind])])
-        df['Breach'][i] = "Yes"
-        df['Covered Entity Type'][i] = mask['Covered Entity Type']
-        df['Individuals Affected'][i] = mask['Individuals Affected']
-        df['Breach Submission Date'][i] = mask['Breach Submission Date']
-        df['Type of Breach'][i] = mask['Type of Breach']
-        df['Location of Breached Information'][i] = mask['Location of Breached Information']
-        df['Business Associate Present'][i] = mask['Business Associate Present']
-        print(df['FAC_NAME'][i])
-print("merged dataframes")
-df.tocsv('MergedHospitalData.csv')"""
+if __name__ == "__main__":
+    find_matches()
+    update_breaches()
+    merge_breached()
